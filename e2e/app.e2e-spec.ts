e615158a546ec5a182e912ed46a7aa2add55ad81
Angular CLI <angular-cli@angular.io>
2017-08-22 21:23:21 -0400

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SundaydiscgolfPage } from './app.po';
+
+describe('sundaydiscgolf App', () => {
+  let page: SundaydiscgolfPage;
+
+  beforeEach(() => {
+    page = new SundaydiscgolfPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
