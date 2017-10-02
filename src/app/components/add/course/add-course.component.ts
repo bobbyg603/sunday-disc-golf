c0a4898fdd9efaa407cdfdf7a05067637d73134a
Bobby Galli <bobbyg603@gmail.com>
2017-10-02 11:09:05 -0400

    Add course validation and style changes.

--- src/app/components/add/course/add-course.component.ts
+++ src/app/components/add/course/add-course.component.ts
@@ -17,13 +17,20 @@ import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@ang
 export class AddCourseComponent implements OnInit {
 
   title = "New Course";
+  currentError = "";
 
   newCourseForm: FormGroup;
 
   nameInputVisible = true;
   addressInputVisible = false;
 
-  constructor(private formBuilder: FormBuilder, private coursesService: CoursesService) { }
+  readonly PAR_NUMBER_PATTERN = "[2-7]";
+  readonly ANY_NUMBER_PATTERN = "^[0-9]+$";
+  readonly PLUS_OR_MINUS_NUMBER_PATTERN = "^[\+\-][0-9]+$";
+
+  constructor(private formBuilder: FormBuilder,
+    private coursesService: CoursesService,
+    private router: Router) { }
 
   ngOnInit() {
     this.createForm();
@@ -33,39 +40,64 @@ export class AddCourseComponent implements OnInit {
     this.newCourseForm = this.formBuilder.group({
       name: ["", Validators.required],
       address: this.formBuilder.group(new Address()),
-      holes: this.formBuilder.array(new Array<Hole>())
+      holes: this.formBuilder.array([])
     })
   }
-
 
   holeInputVisible(index) {
     return this.holes.length - 1 == index;
   }
 
   next() {
-    if(this.nameInputVisible) {
-      this.nameInputVisible = false;
-      this.addressInputVisible = true;
-    } else if(this.addressInputVisible) {
-      this.addressInputVisible = false;
-      this.addHole();
-    } else {
-      this.addHole();
+    if (this.newCourseForm.valid) {
+      if (this.nameInputVisible) {
+        this.nameInputVisible = false;
+        this.addressInputVisible = true;
+      } else if (this.addressInputVisible) {
+        this.addressInputVisible = false;
+        this.addHole();
+      } else {
+        this.addHole();
+      }
     }
   }
 
+  done() {
+    const courseFormAny = <any>this.newCourseForm.value;
+    const name = courseFormAny.name;
+    const holes = courseFormAny.holes.map(hole => new Object({
+      number: +hole.number,
+      par: +hole.par,
+      distance: +hole.distance,
+      elevation: hole.elevation,
+      description: hole.description
+    }));
+    const street = courseFormAny.address.street;
+    const city = courseFormAny.address.city;
+    const state = courseFormAny.address.state;
+    const zip = courseFormAny.address.zip;
+    const newCourse = new Course(name, holes, street, city, state, zip);
+    this.coursesService.create(newCourse).subscribe(response => {
+      console.log(response);
+      this.router.navigate(["/courses"]);
+    });
+  }
+
   addHole() {
-    const holeNumber = this.holes.length + 1;
-    this.holes.push(this.formBuilder.group(new Hole(holeNumber)));
+    this.holes.push(this.formBuilder.group({
+      number: this.holes.length + 1,
+      par: ['', Validators.compose([Validators.required, Validators.pattern(this.PAR_NUMBER_PATTERN)])],
+      distance: ['', Validators.compose([Validators.required, Validators.pattern(this.ANY_NUMBER_PATTERN)])],
+      elevation: ['', Validators.pattern(this.PLUS_OR_MINUS_NUMBER_PATTERN)],
+      description: ['', Validators.nullValidator]
+    }));
   }
 
   get holes(): FormArray {
     return this.newCourseForm.get("holes") as FormArray;
   }
 
-  setHoles(holes: Array<Hole>) {
-    const holesFormGroups = holes.map(hole => this.formBuilder.group(hole));
-    const holesFormArray = this.formBuilder.group(holesFormGroups);
-    this.newCourseForm.setControl("holes", holesFormArray); 
+  get name(): FormControl {
+    return this.newCourseForm.get("name") as FormControl;
   }
 }
