a61a8fec7e45dafb3849fcc929645a14a18c02c9
Bobby Galli <bobbyg603@gmail.com>
2017-10-14 18:45:27 -0400

    Added basic store with stickers.

---
+++ src/app/components/store/store.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store',
+  templateUrl: './store.component.html',
+  styleUrls: ['./store.component.css']
+})
+export class StoreComponent implements OnInit {
+
+  title = "Store"
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
