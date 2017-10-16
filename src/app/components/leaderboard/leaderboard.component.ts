80f7fc335130ed811bc7061434771392e24d8741
Bobby Galli <bobbyg603@gmail.com>
2017-10-16 00:37:17 -0400

    Added placeholders to leaderboard component.

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -15,36 +15,45 @@ export class LeaderboardComponent implements OnInit {
 
   ngOnInit() {
     // TODO BG remove
-    this.statistics.push(new Statistic("Aces", [
-      new PlayerValuePair(new Player("bobbyg603", "password"), 9),
-      new PlayerValuePair(new Player("lil jake", "password"), 10),
-      new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
-    ]));
-    this.statistics.push(new Statistic("Albatrosses", [
-      new PlayerValuePair(new Player("bobbyg603", "password"), 9),
-      new PlayerValuePair(new Player("lil jake", "password"), 10),
-      new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
-    ]));
-    this.statistics.push(new Statistic("Eagles", [
-      new PlayerValuePair(new Player("bobbyg603", "password"), 9),
-      new PlayerValuePair(new Player("lil jake", "password"), 10),
-      new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
-    ]));
-    this.statistics.push(new Statistic("Birdies", [
-      new PlayerValuePair(new Player("bobbyg603", "password"), 9),
-      new PlayerValuePair(new Player("lil jake", "password"), 10),
-      new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
-    ]));
-    this.statistics.push(new Statistic("Pars", [
-      new PlayerValuePair(new Player("bobbyg603", "password"), 9),
-      new PlayerValuePair(new Player("lil jake", "password"), 10),
-      new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
-    ]));
-    this.statistics.push(new Statistic("Average Score", [
-      new PlayerValuePair(new Player("bobbyg603", "password"), 20),
-      new PlayerValuePair(new Player("lil jake", "password"), 19),
-      new PlayerValuePair(new Player("saynotwice", "password"), 18) 
-    ]));
+    this.statistics.push(new Statistic("Coming Soon!", []));
+    this.statistics.push(new Statistic("Wins", []));
+    this.statistics.push(new Statistic("Win Percentage", []));
+    this.statistics.push(new Statistic("Aces", []));
+    this.statistics.push(new Statistic("Double Eagles", []));
+    this.statistics.push(new Statistic("Eagles", []));
+    this.statistics.push(new Statistic("Birdies", []));
+    this.statistics.push(new Statistic("Pars", []));
+    
+    // this.statistics.push(new Statistic("Aces", [
+    //   new PlayerValuePair(new Player("bobbyg603", "password"), 9),
+    //   new PlayerValuePair(new Player("lil jake", "password"), 10),
+    //   new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
+    // ]));
+    // this.statistics.push(new Statistic("Albatrosses", [
+    //   new PlayerValuePair(new Player("bobbyg603", "password"), 9),
+    //   new PlayerValuePair(new Player("lil jake", "password"), 10),
+    //   new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
+    // ]));
+    // this.statistics.push(new Statistic("Eagles", [
+    //   new PlayerValuePair(new Player("bobbyg603", "password"), 9),
+    //   new PlayerValuePair(new Player("lil jake", "password"), 10),
+    //   new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
+    // ]));
+    // this.statistics.push(new Statistic("Birdies", [
+    //   new PlayerValuePair(new Player("bobbyg603", "password"), 9),
+    //   new PlayerValuePair(new Player("lil jake", "password"), 10),
+    //   new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
+    // ]));
+    // this.statistics.push(new Statistic("Pars", [
+    //   new PlayerValuePair(new Player("bobbyg603", "password"), 9),
+    //   new PlayerValuePair(new Player("lil jake", "password"), 10),
+    //   new PlayerValuePair(new Player("saynotwice", "password"), 9001) 
+    // ]));
+    // this.statistics.push(new Statistic("Average Score", [
+    //   new PlayerValuePair(new Player("bobbyg603", "password"), 20),
+    //   new PlayerValuePair(new Player("lil jake", "password"), 19),
+    //   new PlayerValuePair(new Player("saynotwice", "password"), 18) 
+    // ]));
     
   }
 
@@ -54,10 +63,10 @@ class Statistic {
   constructor(public title: string, public rows: Array<PlayerValuePair>) { }
 
   getRowsDescending() {
-    return this.rows.sort((a, b) => b.value - a.value);
+    return this.rows.sort((a, b) => parseInt(b.value) - parseInt(a.value));
   }
 }
 
 class PlayerValuePair {
-  constructor(public player: Player, public value: number) { }
+  constructor(public player: Player, public value: string) { }
 }
